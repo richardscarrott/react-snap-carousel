dce6b219467e75fe65c5fee2464a1c5c9d8b7765
Richard Scarrott <riscarrott@googlemail.com>
2022-12-20 14:23:55 +0000

    Update storybook examples

--- stories/carousel.stories.tsx
+++ stories/carousel.stories.tsx
@@ -1,17 +1,38 @@
-import { Carousel } from './carousel';
+import React, { useLayoutEffect, useRef, useState } from 'react';
+import { Carousel, CarouselItem, CarouselRef } from './carousel';
 
 export default {
   title: 'Carousel',
   component: Carousel
 };
 
-export const Default = {
-  args: {}
+export const Default = () => {
+  const items = Array.from({ length: 18 });
+  return (
+    <Carousel>
+      {items.map((_, i) => (
+        <CarouselItem key={i} bgColor={getColor(i)}>
+          {i + 1}
+        </CarouselItem>
+      ))}
+    </Carousel>
+  );
 };
 
-// export const VariableWidth = {
-//   args: {}
-// };
+export const VariableWidth = () => {
+  const items = [
+    110, 300, 500, 120, 250, 300, 500, 400, 180, 300, 350, 700, 400, 230, 300
+  ];
+  return (
+    <Carousel>
+      {items.map((width, i) => (
+        <CarouselItem key={i} bgColor={getColor(i)} width={width}>
+          {i + 1}
+        </CarouselItem>
+      ))}
+    </Carousel>
+  );
+};
 
 // export const Vertical = {
 //   args: {}
@@ -20,3 +41,65 @@ export const Default = {
 // export const VariableHeight = {
 //   args: {}
 // };
+
+export const AddItems = () => {
+  const carouselRef = useRef<CarouselRef>(null);
+  const [items, setItems] = useState(() => Array.from({ length: 1 }));
+  const addItem = () => {
+    setItems((prev) => [...prev, undefined]);
+  };
+  const removeItem = () => {
+    setItems((prev) => prev.slice(0, -1));
+  };
+  useLayoutEffect(() => {
+    if (!carouselRef.current) {
+      return;
+    }
+    carouselRef.current.refresh();
+  }, [items]);
+  return (
+    <>
+      <Button onClick={() => removeItem()}>Remove Item</Button>
+      <Button onClick={() => addItem()}>Add Item</Button>
+      <Carousel ref={carouselRef}>
+        {items.map((_, i) => (
+          <CarouselItem key={i} bgColor={getColor(i)}>
+            {i + 1}
+          </CarouselItem>
+        ))}
+      </Carousel>
+    </>
+  );
+};
+
+/* Utils */
+
+const getColor = (i: number) => {
+  return `hsl(${i * 12} 100% 50%)`;
+};
+
+interface ButtonProps {
+  readonly children?: React.ReactNode;
+  readonly onClick?: () => void;
+}
+
+const Button = ({ children, onClick }: ButtonProps) => {
+  return (
+    <button
+      style={{
+        background: '#04bfad',
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        color: '#fff',
+        padding: 10,
+        marginRight: 5,
+        marginBottom: 20,
+        fontSize: 16,
+        borderRadius: 5
+      }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
--- stories/carousel.tsx
+++ stories/carousel.tsx
@@ -1,52 +1,91 @@
-import React from 'react';
+import React, { useImperativeHandle } from 'react';
 import classNames from 'classnames';
 import { useSnapCarousel } from '../src/use-snap-carousel';
 import './reset.css';
 const styles = require('./carousel.module.css');
 
-export const Carousel = () => {
-  const { scrollRef, next, prev, goTo, pages, activePageIndex, refresh } =
-    useSnapCarousel();
-  return (
-    <div className={styles.root}>
-      <ul className={styles.scroll} ref={scrollRef}>
-        {Array.from({ length: 100 }).map((_, i) => (
-          <li
-            className={styles.item}
-            key={i}
-            style={{ backgroundColor: getColor(i) }}
-          >
-            Item {i}
-          </li>
-        ))}
-      </ul>
-      <div className={styles.pageIndicator}>
-        {activePageIndex + 1} / {pages.length}
-      </div>
-      <button className={styles.prevButton} onClick={() => prev()}>
-        ⬅️ Prev
-      </button>
-      <button className={styles.nextButton} onClick={() => next()}>
-        Next ➡️
-      </button>
-      <ol className={styles.pagination}>
-        {pages.map((_, i) => (
-          <li className={styles.paginationItem} key={i}>
-            <button
-              className={classNames(styles.paginationButton, {
-                [styles.paginationButtonActive]: i === activePageIndex
-              })}
-              onClick={() => goTo(i)}
-            >
-              {i + 1}
-            </button>
-          </li>
-        ))}
-      </ol>
-    </div>
-  );
-};
+/**
+ * This is an example Carousel built on top of `useSnapCarousel`
+ */
 
-const getColor = (i: number) => {
-  return `hsl(${i * 10} 100% 50%)`;
+export interface CarouselProps {
+  readonly children?: React.ReactNode;
+}
+
+export interface CarouselRef {
+  readonly refresh: () => void;
+}
+
+export const Carousel = React.forwardRef<CarouselRef, CarouselProps>(
+  ({ children } = {}, ref) => {
+    const { scrollRef, next, prev, goTo, pages, activePageIndex, refresh } =
+      useSnapCarousel();
+
+    useImperativeHandle(ref, () => ({ refresh }));
+
+    return (
+      <div className={styles.root}>
+        <ul className={styles.scroll} ref={scrollRef}>
+          {children}
+        </ul>
+        <div className={styles.pageIndicator}>
+          {activePageIndex + 1} / {pages.length}
+        </div>
+        <div className={styles.controls}>
+          <button
+            disabled={activePageIndex === 0}
+            onClick={() => prev()}
+            className={styles.prevButton}
+          >
+            {String.fromCharCode(8592)}
+          </button>
+          <ol className={styles.pagination}>
+            {pages.map((_, i) => (
+              <li
+                key={i}
+                className={classNames(styles.paginationItem, {
+                  [styles.paginationItemActive]: i === activePageIndex
+                })}
+              >
+                <button
+                  className={styles.paginationButton}
+                  onClick={() => goTo(i)}
+                >
+                  {i + 1}
+                </button>
+              </li>
+            ))}
+          </ol>
+          <button
+            disabled={activePageIndex === pages.length - 1}
+            onClick={() => next()}
+            className={styles.nextButton}
+          >
+            {String.fromCharCode(8594)}
+          </button>
+        </div>
+      </div>
+    );
+  }
+);
+
+export interface CarouselItemProps {
+  readonly bgColor: string;
+  readonly width?: number;
+  readonly children?: React.ReactNode;
+}
+
+export const CarouselItem = ({
+  bgColor,
+  width,
+  children
+}: CarouselItemProps) => {
+  return (
+    <li
+      className={styles.item}
+      style={{ backgroundColor: bgColor, width: width ?? '' }}
+    >
+      {children}
+    </li>
+  );
 };
